89e660db1b8cbd2f80043db4c8b99bb7af11679f
Shivam Mishra <shivaymishraeee@gmail.com>
2024-01-21 20:20:21 +0530

    its done now

--- Project -1/script.js
+++ Project -1/script.js
@@ -47,7 +47,7 @@ function circleskewanimation(){
         circlefollowmouse(xscale, yscale);
 
         timeout = setTimeout(()=>{
-            document.querySelector('#minicircle').style.transform = `translate(${dets.clientX }px, ${dets.clientY}px) scale(1, 1) `;
+            document.querySelector('#minicircle').style.transform = `translate(${dets.clientX-5 }px , ${dets.clientY-5}px) scale(1, 1) `;
         },100);
 
     });
@@ -55,50 +55,10 @@ function circleskewanimation(){
 
 function circlefollowmouse(xscale, yscale){
     window.addEventListener("mousemove", function(e){
-        document.querySelector('#minicircle').style.transform = `translate(${e.clientX}px, ${e.clientY}px) scale(${xscale}, ${yscale}) `;
+        document.querySelector('#minicircle').style.transform = `translate(${e.clientX - 5}px, ${e.clientY - 5}px) scale(${xscale}, ${yscale}) `;
     });
+    
 }
-
-// document.querySelectorAll('.elem')
-// .forEach(function(elem){
-//     var rotate = 0;
-//     var diffrot = 0;
-//     elem.addEventListener('mouseleave',function(dets){
-//         let diff = dets.clientY - elem.getBoundingClientRect().top;
-//         diffrot = dets.clientX - rotate;
-//         rotate = dets.clientX;
-//         gsap.to(elem.querySelector("img"),{
-//             opacity:0,
-//             ease: Power3,
-//             duration:0.5,
-//             transform:'scale(1)',
-//         })
-//     })
-//     elem.addEventListener('mousemove',function(dets){
-//         let diff = dets.clientY - elem.getBoundingClientRect().top;
-//         diffrot = dets.clientX - rotate;
-//         rotate = dets.clientX;
-//         gsap.to(elem.querySelector("img"),{
-//             opacity:1,
-//             ease: Power3,
-//             top:diff,
-//             left:dets.clientX,
-//             rotate:gsap.utils.clamp(-20,20,diffrot*0.5),
-//         })
-//     })
-//     elem.addEventListener('mouseenter',function(dets){
-//         let diff = dets.clientY - elem.getBoundingClientRect().top;
-//         diffrot = dets.clientX - rotate;
-//         rotate = dets.clientX;
-//         gsap.to(elem.querySelector("img"),{
-//             opacity:1,
-//             ease: Power3,
-//             top:diff,
-//             left:dets.clientX,
-//             rotate:gsap.utils.clamp(-20,20,diffrot*0.5),
-//         })
-//     })
-// })
 
 document.querySelectorAll('.elem').forEach(function(elem) {
     var rotate = 0;
@@ -111,7 +71,6 @@ document.querySelectorAll('.elem').forEach(function(elem) {
             opacity: 0,
             ease: Power3,
             duration: 0.5,
-            transform: 'scale(1)',
         })
     })
 
